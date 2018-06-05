a0e6e0ab0df2bddab895f78bc6d4ee004e0d1298
朴野归踪 <2472906920@qq.com>
2018-06-05 19:54:31 +0800

    add
    add

--- E.tools前端工具框架/webpack/5.middleware.js
+++ E.tools前端工具框架/webpack/5.middleware.js
@@ -1,21 +1,21 @@
 
 
 /**
- * @param {webpack}
- * path:  文件输出路径;
- * publicPath:  指定所有资源(css/js/img)的基础路径;
+ * TITLE:   webpack
+ * INTRO:   path:  文件输出路径;
+ *          publicPath:  指定所有资源(css/js/img)的基础路径;
  */
 
 
 
 
 /**
- * @param {webpack-dev-middleware}
- * 定义:  webpack-dev-middleware是express的一个中间件;
- * 运作:  读取webpack.config,将文件输出到内存中,因此构建速度很快;
- *        webpack能把所有资源当做模块来处理,可以向客户端反馈各种资源,所以可以代替http服务器
- * 区别:  定制化程度更高;
- * 参数:  publicPath: 资源在内存输出的目录
+ * TITLE:   webpack-dev-middleware
+ * INTRO:   publicPath: 资源在内存输出的目录
+ * DETIAL:  webpack-dev-middleware是express的一个中间件;
+ *          读取webpack.config,将文件输出到内存中,因此构建速度很快;
+ *          webpack能把所有资源当做模块来处理,可以向客户端反馈各种资源,所以可以代替http服务器
+ *          定制化程度更高; 
  */
 var server  = express()
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
@@ -31,32 +31,60 @@ server.use(devMiddleware)
 
 
 
-/**
- * @param {webpack-hot-middleware}
- * 定义: 结合webpack-dev-middleware的中间件；
- * 运作: 实现浏览器的无刷新更新（hot reload），HMR和热加载的区别是：热加载是刷新整个页面
- * 区别: 定制化成都差，很难编写后端服务，进行整合。
- */
-var hotMiddleware = require('webpack-hot-middleware')(compiler)
-
 
 
 /**
- * @param {webpack-dev-server}
- * 定义:  express + webpack-dev-middleware的中间件;
- * 运作:  同webpack-dev-middleware一样
- * 区别:  定制化成都差,很难编写后端服务,进行整合
+ * TITLE:   webpack-hot-middleware
+ * DETIAL:  结合webpack-dev-middleware的中间件；
+ *          实现浏览器的无刷新更新（hot reload），HMR和热加载的区别是：热加载是刷新整个页面
+ *          定制化成都差，很难编写后端服务，进行整合。
  */
+var compiler        = webpack(webpackConfig)
+var hotMiddleware   = require('webpack-hot-middleware')(compiler)
+
+compiler.plugin('compilation', function(compilation) {
+  compilation.plugin('html-webpack-plugin-after-emit', function(data, cb) {
+    hotMiddleware.publish({
+      action: 'reload'
+    })
+    cb()
+  })
+})
+
+server.use(hotMiddleware)
+
+
+/**
+ * TITLE:   http-proxy-middleware
+ * DETIAL:  
+ */
+var proxyMiddleware = require('http-proxy-middleware')
+server.use(proxyMiddleware(context, {
+  target: proxypath,
+  changeOrigin: true,
+}))
+
+
+
+
 
 
 
 /**
- * @param {http-proxy-middleware}
- * 定义:  
- * 运作:  
- * 区别:  
+ * TITLE:   webpack-dev-server
+ * DETIAL:  express + webpack-dev-middleware的中间件;
+ *          同webpack-dev-middleware一样
+ *          定制化成都差,很难编写后端服务,进行整合
  */
-server.use(proxyMiddleware(context, options))
+
+
+
+
+
+
+
+
+
 
 
 
